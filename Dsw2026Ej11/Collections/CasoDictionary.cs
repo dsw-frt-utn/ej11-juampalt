e772b4f182f2a6589fc63ed7b956419beb531451
Juan Pablo <chulolules@gmail.com>
2026-05-22 01:28:18 -0300

    CasoDictionary completado

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,25 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    Dictionary<int, Alumno> diccionario = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        diccionario.Add(alumno.Id, alumno);
+    }
+
+    public Alumno BuscarAlumnoPorID(int legajo)
+    {
+        return diccionario[legajo];
+    }
+
+    public Dictionary<int, Alumno> RetornarDic()
+    {
+        return diccionario;
+    }
+
+    public void BorrarAlumno(int legajo)
+    {
+        diccionario.Remove(legajo);
+    }
 }
